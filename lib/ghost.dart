c2f0834a97598c05ab38f226afd7368dcd2acd16
alvinle2901 <bach938@gmail.com>
2024-01-31 15:18:42 +0200

    initial commit

---
+++ lib/ghost.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Ghost extends StatelessWidget {
+  const Ghost({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(4.0),
+      child: Image.asset(
+        'lib/images/ghost.png',
+      ),
+    );
+  }
+}
